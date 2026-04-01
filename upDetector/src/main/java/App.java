9ffa11de92ceabe692b6ee894047bb839d3f8428
24DP2MKras <A240364MK@rvt.lv>
2026-04-01 06:13:08 +0000

    check in morning!

---
+++ upDetector/src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public String todo() {
+        return "This method must mean theres something to do";
+        //make a main menu screen for the user
+        //make it so the option the user makes redirect them to somewhere (not implemented yet)
+        //make it all correctly row by row so the output of main menu isnt scrambled
+        //remember to be use java!
+    }
+}
